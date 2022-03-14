3d0cc0d319d4ba028605c043edb7e803c0e2cd5b
Andrew-Sem <avvyayvayva@gmail.com>
2022-03-14 13:14:51 +0300

    added api

---
+++ pages/api/echo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// тут работаем на сервере
+export default function echo(req, res) {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.end(
+    JSON.stringify({
+      message: req.query.message ?? "Base message",
+    })
+  );
+}
